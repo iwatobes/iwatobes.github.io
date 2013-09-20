9a58add7b53082b14f0889e8913e94f6c62d9cd4
Nguyen Viet Thinh <nvthinh@hoiio.com>
2013-09-20 15:44:02 +0800

    Mark this repo as production repo. Will only contain production code

--- README.md
+++
@@ -1,4 +0,0 @@
-# About
-
-This is the source for my personal website. An attempt to use angular+require to develop a personal blogging space
-
--- js/libs/jquery/jquery.js
+++
@@ -1,8 +0,0 @@
-if( !window.jQuery ) {
-  define(['Console', 'order!libs/jquery/jquery-min'], function (Console) {
-    Console.group("Entering jQuery module.");
-    Console.info("jQuery: ", $);
-    Console.groupEnd();
-    return $;
-  });
-}
